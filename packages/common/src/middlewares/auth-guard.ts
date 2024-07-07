a59d1cbaeebf35c4d06e209d6f5be3714f6a2403
Daniele Tortora <tortora87@gmail.com>
2024-07-07 22:13:11 +0200

    chore: release

--- packages/common/src/middlewares/auth-guard.ts
+++ packages/common/src/middlewares/auth-guard.ts
@@ -11,39 +11,37 @@ declare global {
   }
 }
 
-export const authGuardMiddleware = (jwtService: JWTService) => async (
-  req: Request,
-  _res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { token } = req.session ?? {};
+export const authGuardMiddleware =
+  (jwtService: JWTService) =>
+  async (req: Request, _res: Response, next: NextFunction) => {
+    try {
+      const { token } = req.session ?? {};
 
-    // check if token exists
-    if (!token) {
-      console.log("No token found");
-      return next(new UnauthorizedError());
+      // check if token exists
+      if (!token) {
+        console.log("No token found");
+        return next(new UnauthorizedError());
+      }
+
+      // verify JWT signature
+      const payload = jwtService.verify(token);
+      if (!payload) {
+        console.log("Invalid token");
+        req.session = null;
+        return next(new UnauthorizedError());
+      }
+
+      // retrieve user from DB
+      const userDoc = await User.findOne({ email: payload.email });
+
+      if (!userDoc) {
+        return next(new UnauthorizedError());
+      }
+
+      req.currentUser = userDoc;
+      return next();
+    } catch (error) {
+      console.log("Middlwere error: ", error);
+      return next(error);
     }
-
-    // verify JWT signature
-    const payload = jwtService.verify(token);
-    if (!payload) {
-      console.log("Invalid token")
-      req.session = null;
-      return next(new UnauthorizedError());
-    }
-
-    // retrieve user from DB
-    const userDoc = await User.findOne({ email: payload.email });
-
-    if (!userDoc) {
-      return next(next(new UnauthorizedError()));
-    }
-
-    req.currentUser = userDoc;
-    return next();
-  } catch (error) {
-    console.log(error);
-    return next(error);
-  }
-};
+  };
