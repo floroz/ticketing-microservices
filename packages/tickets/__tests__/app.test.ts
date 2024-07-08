1a263fb753b8ff2aef515c5bf05a9a987440ce42
Daniele Tortora <tortora87@gmail.com>
2024-07-08 09:37:09 +0200

    test: wip

--- packages/tickets/__tests__/app.test.ts
+++ packages/tickets/__tests__/app.test.ts
@@ -1,37 +1,48 @@
-import supertest from "supertest";
+import request from "supertest";
 import { app } from "../src/app";
 import { it, expect } from "vitest";
 
+const email = "test@test.com";
+const password = "password";
+
 it("returns 200 for GET /api/tickets", async () => {
-  const response = await supertest(app).get("/api/tickets");
-  expect(response.status).toBe(200);
+  const response = await request(app).get("/api/tickets").expect(200);
   expect(response.body.message).toBe("Hello, ticket!");
 });
 
 it("returns 200 for GET /api/tickets/:id", async () => {
-  const response = await supertest(app).get("/api/tickets/1");
-  expect(response.status).toBe(200);
+  const response = await request(app).get("/api/tickets/1").expect(200);
   expect(response.body.message).toBe("Hello, 1");
 });
 
-it.only("returns 401 for POST /api/tickets - when unauthorized", async () => {
-  const response = await supertest(app).post("/api/tickets");
-  expect(response.status).toBe(401);
+it("returns 401 for POST /api/tickets - when unauthorized", async () => {
+  await request(app).post("/api/tickets").expect(401);
 });
 
-it.skip("returns 401 for PUT /api/tickets/:id - when unauthorized", async () => {
-  const response = await supertest(app).put("/api/tickets/1");
-  expect(response.status).toBe(401);
+it("returns 401 for PUT /api/tickets/:id - when unauthorized", async () => {
+  await request(app).put("/api/tickets/1").expect(401);
 });
 
-it.skip("returns 201 for POST /api/tickets", async () => {
-  const response = await supertest(app)
+it("returns 201 for POST /api/tickets", async () => {
+  await request(app)
     .post("/api/tickets")
-    .set("Authorization", "Bearer 123");
-  expect(response.status).toBe(201);
+    .set("Cookie", "1234")
+    .send({})
+    .expect(201);
 });
 
-it.skip("returns 200 for PUT /api/tickets/:id", async () => {
-  const response = await supertest(app).put("/api/tickets/1");
-  expect(response.status).toBe(200);
+it("returns 200 for PUT /api/tickets/:id", async () => {
+  const signup = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signup.get("Set-Cookie"))
+    .send({})
+    .expect(201);
 });
