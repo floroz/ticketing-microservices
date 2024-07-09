12a085028154f57433f36fcabd857d34d6ab8336
Daniele Tortora <tortora87@gmail.com>
2024-07-09 09:02:39 +0200

    feat: create ticket model

--- packages/common/src/models/ticket.ts
+++ packages/common/src/models/ticket.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 type TicketCreationPayload = {
+  title: string;
+  price: number;
+  currency: string;
+  userId: string;
 };
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -8,15 +12,49 @@ interface TicketModel extends mongoose.Model<TicketDoc> {
 }
 
 export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  currency: string;
+  userId: string;
+  /**
+   * ISO Timestamp
+   */
+  createdAt: string;
+  /**
+   * ISO Timestamp
+   */
+  updatedAt: string;
 }
 
 const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
+  updatedAt: {
+    type: String,
+    required: true,
+  },
 });
 
 ticketSchema.set("toJSON", {
   transform: (doc: any, ret: any) => {
-    delete ret.password;
-    delete ret.__v;
     ret.id = ret._id;
     delete ret._id;
   },
@@ -27,6 +65,8 @@ ticketSchema.statics.build = (payload: TicketCreationPayload) => {
 };
 
 ticketSchema.pre("save", async function (done) {
+  this.createdAt = new Date().toISOString();
+  this.updatedAt = new Date().toISOString();
   done();
 });
 
