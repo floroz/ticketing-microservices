149d713d6e38f5f9566bcde57856fee47f9688d9
Daniele Tortora <tortora87@gmail.com>
2024-07-14 15:31:09 +0200

    fix: missing export

--- packages/common/src/models/user.ts
+++ packages/common/src/models/user.ts
@@ -10,7 +10,7 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(payload: UserCreationPayload): UserDoc;
 }
 
-interface UserDoc extends mongoose.Document {
+export interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
   role: "admin" | "user";
